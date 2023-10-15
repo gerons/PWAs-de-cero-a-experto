c024593159b5698fe535099945581cea05019556
Gero Nñz <gerons69@gmail.com>
2023-10-15 16:41:26 -0600

    ..

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,26 +1,49 @@
+const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_DYNAMIC_NAME = 'dynamic-v1'
 
+const CACHE_INMUTABLE_NAME = 'inmutable-v1'
 
 self.addEventListener('install', e => {
-
-    const crearCache = caches.open('cache-1')
+    const cacheEstatico = caches.open( CACHE_STATIC_NAME )
     .then(cache => {
         return cache.addAll([
             '/04-cache-offline/',
             '/04-cache-offline/index.html',
-            '/04-cache-offline/ccs/style.css',
+            '/04-cache-offline/css/style.css',
             '/04-cache-offline/img/main.jpg',
-            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
             '/04-cache-offline/js/app.js'
         ])
     })
 
-    e.waitUntil( crearCache )
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
+    .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' ))
+
+    e.waitUntil( Promise.all([cacheEstatico, cacheInmutable ]))
 
 })
 
 self.addEventListener('fetch', e => {
 
+    // 2 - Cache with network fallback
+    // busca primero en cache y si no encuentra va a internet
+    const respuesta = caches.match( e.request )
+    .then(res => {
+        if (res) return res
+
+        return fetch( e.request )
+        .then(resp => {
+            caches.open( CACHE_DYNAMIC_NAME )
+            .then(cache => {
+                cache.put( e.request, resp)
+            })
+
+            return resp.clone()
+        })
+    })
+
+    e.respondWith( respuesta )
+
     // 1 - Cache only
     // cuando toda la aplicacion carga desde cache
-    e.respondWith( caches.match( e.request ) )
+    // e.respondWith( caches.match( e.request ) )
 })
