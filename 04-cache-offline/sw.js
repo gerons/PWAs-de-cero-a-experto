32eb1db24ca03b3979655022d0a8045e6a86d72a
Geronimo Nuñez <gerons69@gmail.com>
2023-10-17 15:56:45 -0600

    Cache & Network Race

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -26,6 +26,7 @@ self.addEventListener('install', e => {
             '/04-cache-offline/index.html',
             '/04-cache-offline/css/style.css',
             '/04-cache-offline/img/main.jpg',
+            '/04-cache-offline/img/no-img.jpg',
             '/04-cache-offline/js/app.js'
         ])
     })
@@ -39,23 +40,53 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
     // ==============================================
-    // 3- Cache with network update   (Stale While Revalidate)
+    // 5- Cache & Network Race 
+    // ==============================================
+    const respuesta = new Promise((resolve, reject) => {
+        let rechazada = false
+
+        const falloUnaVez = () => {
+            if (rechazada) {
+                if (/\.(png|jpg)$/i.test(e.request.url)) {
+                    resolve(caches.match('/04-cache-offline/img/no-img.jpg'))
+                } else {
+                    reject('No se encontró respuesta')
+                }
+
+            } else {
+                rechazada = true
+            }
+        }
+
+        fetch(e.request).then(res => {
+            res.ok ? resolve(res) : falloUnaVez()
+        }).catch( falloUnaVez )
+
+        caches.match(e.request).then(res => {
+            res ? resolve(res) : falloUnaVez()
+        }).catch( falloUnaVez )
+    })
+
+    e.respondWith( respuesta )
+
+    // ==============================================
+    // 4- Cache with network update   (Stale While Revalidate)
     // Cuando el rendimiento es critico
     // el cache siempre estará un paso atraz 
     // (primero mostramos cache y luego actualizamos el mismo)
     // ==============================================
-    if (e.request.url.includes('bootstrap')) {
-        return e.respondWith(caches.match(e.request))
-    }
+    // if (e.request.url.includes('bootstrap')) {
+    //     return e.respondWith(caches.match(e.request))
+    // }
     
-    const respuesta = caches.open(CACHE_STATIC_NAME)
-    .then(cache => {
-        fetch(e.request).then(newREs => cache.put(e.request, newREs))
+    // const respuesta = caches.open(CACHE_STATIC_NAME)
+    // .then(cache => {
+    //     fetch(e.request).then(newREs => cache.put(e.request, newREs))
 
-        return cache.match(e.request)
-    })
+    //     return cache.match(e.request)
+    // })
 
-    e.respondWith( respuesta )
+    // e.respondWith( respuesta )
 
     // ==============================================
     // 3- Cache Network with cache fallback (Network First)
